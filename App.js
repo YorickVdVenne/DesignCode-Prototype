0a26d786f2d992b0bf96d8bd588cb8a1d2d7d5e9
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2021-04-06 11:44:23 +0200

    Adding Geofencing

--- App.js
+++ App.js
@@ -13,18 +13,20 @@ import MapView, {PROVIDER_GOOGLE, Marker, Circle, Polygon} from 'react-native-ma
 import Geolocation from 'react-native-geolocation-service';
 import PushNotification from "react-native-push-notification";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import GeoFencing from 'react-native-geo-fencing';
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
         currentLocation: {lat: 52.130363, lng: 4.334120},
-        nextLocation: {lat: 52.132933, lng: 4.333433},
+        nextLocation: {lat: 52.128053, lng: 4.333560},
         previousLocation: null,
         isEnter: false,
         hasPreviousLocation: false
     }
 
+    // Pushnotification configure settings
     PushNotification.configure({
       onRegister: function (token) {
         console.log("TOKEN:", token);
@@ -47,20 +49,19 @@ export default class App extends React.Component {
       popInitialNotification: true,
       requestPermissions: true,
     });
+    
+    //Create channel for pushnotifications to use
     PushNotification.createChannel(
       {
         channelId: "channel-id", // (required)
         channelName: "My channel", // (required)
-        channelDescription: "A channel to categorise your notifications", // (optional) default: undefined.
-        playSound: false, // (optional) default: true
-        soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
-        importance: 4, // (optional) default: 4. Int value of the Android notification importance
-        vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
+        channelDescription: "A channel to categorise your notifications",
       },
     );
   }
 
   sendMessage = () => {
+    //Sending a local notification
     PushNotification.localNotification({
       channelId: "channel-id", 
       title: "Je bent van het pad af gegaan!", // (optional)
@@ -68,31 +69,46 @@ export default class App extends React.Component {
     });
   }
 
-  polygon = [
-    {
-      'latitude': 52.127817,
-      'longitude': 4.336369,
-    },
-    {
-      'latitude': 52.132933,
-      'longitude': 4.333433,
-    },
-    {
-      'latitude': 52.130284,
-      'longitude': 4.332875,
-    }
-  ]
-  
-  handleUserLocation = () => {
-    Geolocation.getCurrentPosition(pos => {
-      alert(JSON.stringify(pos))
-    })
+  componentDidMount() {
+    // Create data for polygon
+    const polygon = [
+      { lat: 52.127817, lng: 4.336369 },
+      { lat: 52.132933, lng: 4.333433 },
+      { lat: 52.130284, lng: 4.332875 },
+      { lat: 52.127817, lng: 4.336369 },
+    ];
+
+    // Get current position of user
+    Geolocation.getCurrentPosition(
+      (position) => {
+        let yourRealLocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        let currentLocation = {
+          lat: this.state.currentLocation.lat,
+          lng: this.state.currentLocation.lng
+        }
+        let nextLocation = {
+          lat: this.state.nextLocation.lat,
+          lng: this.state.nextLocation.lng
+        }
+        
+        // Calculating if location is inside the polygon
+        GeoFencing.containsLocation(nextLocation, polygon)
+          .then(() => console.log('Je bevindt je nog op het pad, ga zo door!'))
+          .catch(() => console.log('Hey! Je bent van het pad af, heel gauw terug gaan!'))
+      },
+      (error) => alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
   }
 
   onPressHandler = () => {
-    this.state.isEnter = true
     this.sendMessage()
+    this.state.currentLocation = this.state.nextLocation
   }
+
   storeData = async () => {
     try {
       console.log('Storing location...')
@@ -107,14 +123,20 @@ export default class App extends React.Component {
     try {
       console.log('Getting previous location...')
       const jsonValue = await AsyncStorage.getItem('location')
-      const data = jsonValue != null ? JSON.parse(jsonValue) : null;
-      this.state.previousLocation = data
+      this.state.previousLocation = jsonValue != null ? JSON.parse(jsonValue) : null;
       this.state.hasPreviousLocation = true
       console.log('Got previous location!')
     } catch(e) {
       console.warn(e)
     }
   }
+
+  polygon = [
+    {'latitude': 52.127817, 'longitude': 4.336369,},
+    {'latitude': 52.132933, 'longitude': 4.333433,},
+    {'latitude': 52.130284, 'longitude': 4.332875,},
+    {'latitude': 52.127817, 'longitude': 4.336369,}
+  ]
 
   render() {
     return (
@@ -127,10 +149,9 @@ export default class App extends React.Component {
           longitude: 4.336369,
           latitudeDelta: 0.009, 
           longitudeDelta: 0.015,
-        }}
-        >
+        }}>
+
         <Polygon coordinates={this.polygon} strokeWidth={2} strokeColor={'red'}/>
-        <Circle center={{latitude: 52.127817, longitude: 4.336369}} radius={50} strokeWidth={2} strokeColor={'red'}/>
         <Marker title={'Current location'} coordinate={{ latitude : this.state.currentLocation.lat , longitude : this.state.currentLocation.lng }} />
         {this.state.hasPreviousLocation ? <Marker title={'Previous location'} coordinate={{ latitude : this.state.previousLocation.lat , longitude : this.state.previousLocation.lng }} /> : null}
       </MapView>
--- android/app/src/main/java/com/designcode/MainApplication.java
+++ android/app/src/main/java/com/designcode/MainApplication.java
@@ -13,6 +13,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage; 
+import com.surialabs.rn.geofencing.GeoFencingPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
